8bce65cc11941723cbb84c51380b469fcc7f36c5
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-06-18 16:01:00 -0400

    started to make the main grid and navbar mobile responsive

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//heeeeeeh
+let hamburger = document.querySelector(".hamburger");
+let ul = document.getElementById("navUl");
+
+hamburger.addEventListener("click", () => {
+    ul.classList.toggle("display");
+});
