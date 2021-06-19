c00b8861038e244231cd500a88b1fc0f44acd2f2
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-06-19 02:50:09 -0400

    more work on the mobile navbar

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let hamburger = document.querySelector(".hamburger");
-let ul = document.getElementById("navUl");
+const hamburger = document.querySelector(".hamburger");
+const ul = document.getElementById("navUl");
+const ulCorner = document.querySelector(".ul__corner-piece");
+const body = document.querySelector("body");
+const arrow = document.getElementById("arrow");
+
+const mobileMQ = window.matchMedia("(max-width: 850px)");
 
 hamburger.addEventListener("click", () => {
     ul.classList.toggle("display");
+    ulCorner.classList.toggle("display");
+    body.classList.toggle("display");
+    arrow.classList.toggle("display");
 });
