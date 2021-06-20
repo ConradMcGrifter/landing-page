f19c69428ec23e4702a786d7bd87bed999784e4a
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-06-20 02:00:54 -0400

    the last commit fixed the image problem.
    this commit I added a opacity transition on the hamburger when clicked

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ const ulCorner = document.querySelector(".ul__corner-piece");
 const body = document.querySelector("body");
 const arrow = document.getElementById("arrow");
 
-const mobileMQ = window.matchMedia("(max-width: 850px)");
-
 hamburger.addEventListener("click", () => {
     ul.classList.toggle("display");
     ulCorner.classList.toggle("display");
     body.classList.toggle("display");
     arrow.classList.toggle("display");
+    hamburger.classList.toggle("display");
 });
